a5dabf8073e045dfbc88cf3b7af7082d56795efa
johneayala <johneayala@gmail.com>
2018-05-28 18:09:14 +0000

    Added case and systemd_unit block for Ubuntu

--- 2_Tomcat/tomcat.rb
+++ 2_Tomcat/tomcat.rb
@@ -69,39 +69,76 @@ end
 end
 
 # Install Systemd unit file & trigger reload of systemd-daemon
-systemd_unit 'tomcat.service' do
-  content <<-EOU.gsub(/^\s+\|/, '')
-  |[Unit]
-  |Description=Apache Tomcat Web Application Container
-  |After=syslog.target network.target
-  |
-  |[Service]
-  |Type=forking
-  |
-  |Environment=JAVA_HOME=/usr/lib/jvm/jre
-  |Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid
-  |Environment=CATALINA_HOME=/opt/tomcat
-  |Environment=CATALINA_BASE=/opt/tomcat
-  |Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
-  |Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
-  |
-  |ExecStart=/opt/tomcat/bin/startup.sh
-  |ExecStop=/bin/kill -15 $MAINPID
-  | 
-  |User=tomcat
-  |Group=tomcat
-  |UMask=0007
-  |RestartSec=10
-  |Restart=always
-  |
-  |[Install]
-  |WantedBy=multi-user.target
-  |
-  EOU
+case node['platform_family']
+when 'rhel'
+  systemd_unit 'tomcat.service' do
+    content <<-EOU.gsub(/^\s+\|/, '')
+    |[Unit]
+    |Description=Apache Tomcat Web Application Container
+    |After=syslog.target network.target
+    |
+    |[Service]
+    |Type=forking
+    |
+    |Environment=JAVA_HOME=/usr/lib/jvm/jre
+    |Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid
+    |Environment=CATALINA_HOME=/opt/tomcat
+    |Environment=CATALINA_BASE=/opt/tomcat
+    |Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
+    |Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
+    |
+    |ExecStart=/opt/tomcat/bin/startup.sh
+    |ExecStop=/bin/kill -15 $MAINPID
+    | 
+    |User=tomcat
+    |Group=tomcat
+    |UMask=0007
+    |RestartSec=10
+    |Restart=always
+    |
+    |[Install]
+    |WantedBy=multi-user.target
+    |
+    EOU
+    action :create
+    triggers_reload true
+  end
 
-  action :create
-  triggers_reload true
+when 'debian'
+  systemd_unit 'tomcat.service' do
+    content <<-EOU.gsub(/^\s+\|/, '')
+    |[Unit]
+    |Description=Apache Tomcat Web Application Container
+    |After=syslog.target network.target
+    |
+    |[Service]
+    |Type=forking
+    |
+    |Environment=JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/jre
+    |Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid
+    |Environment=CATALINA_HOME=/opt/tomcat
+    |Environment=CATALINA_BASE=/opt/tomcat
+    |Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
+    |Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
+    |
+    |ExecStart=/opt/tomcat/bin/startup.sh
+    |ExecStop=/bin/kill -15 $MAINPID
+    | 
+    |User=tomcat
+    |Group=tomcat
+    |UMask=0007
+    |RestartSec=10
+    |Restart=always
+    |
+    |[Install]
+    |WantedBy=multi-user.target
+    |
+    EOU
+    action :create
+    triggers_reload true
+  end
 end
+
 
 # Start and enable tomcat service
 service 'tomcat' do
